b2f48b6623885d7f11ef8a244f4bd2f1ad7542d8
Gorlenochka <koshkalena88@gmail.com>
2022-09-15 15:00:23 +0300

    Added API integration

--- src/App.js
+++ src/App.js
@@ -1,108 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 export default function App() {
-  let weatherForecast = {
-    city: "Kiev",
-    temperature: "15",
-    date: "06.09.2022",
-    time: "21.00",
-    wind: "4",
-    precipitation: "45",
-    pressure: "1022",
-  };
-  return (
-    <div className="App">
-      <div className="Border">
-        <div className="PreviouslySearch">
-          <p className="PreviouslyCities">Previously searched cities:</p>
-          <a href="/" className="PreviouslyCity">
-            Kyiv
-          </a>
-          <a href="/" className="PreviouslyCity">
-            London
-          </a>
-          <a href="/" className="PreviouslyCity">
-            Warsaw
-          </a>
-        </div>
-        <div className="Search">
-          <form>
-            <input
-              type="text"
-              placeholder="Search a city"
-              className="SearchACity"
-            />
-            <input type="submit" value="Let's go!" className="Go" id="go" />
-            <input
-              type="button"
-              value="Current"
-              className="Current"
-              id="current"
-            />
-          </form>
-        </div>
-        <div className="MainCity">
-          <h1 id="search-city">{weatherForecast.city}</h1>
-          <div className="Today">
-            <p className="DateToday">{weatherForecast.date}</p>
-            <p className="Time">{weatherForecast.time}</p>
+  const [weatherForecast, setWeatherForecast] = useState({ ready: false });
+
+  function forecastResponse(response) {
+    setWeatherForecast({
+      ready: true,
+      city: "Paris",
+      date: "06.09.2022",
+      time: "21.00",
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      pressure: response.data.main.pressure,
+      icon: response.data.weather[0].icon,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  if (weatherForecast.ready) {
+    return (
+      <div className="App">
+        <div className="Border">
+          <div className="PreviouslySearch">
+            <p className="PreviouslyCities">Previously searched cities:</p>
+            <a href="/" className="PreviouslyCity">
+              Kyiv
+            </a>
+            <a href="/" className="PreviouslyCity">
+              London
+            </a>
+            <a href="/" className="PreviouslyCity">
+              Warsaw
+            </a>
           </div>
-          <div className="Container">
-            <div className="Row">
-              <div className="Col">
-                <img
-                  src="http://openweathermap.org/img/wn/10d@2x.png"
-                  alt="shower rain"
-                  className="ImageWeatherToday"
-                />
-              </div>
-              <div className="Col">
-                <p className="TemperatureToday">
-                  {weatherForecast.temperature}
-                  <sup className="CelsiusFahrenheit">°С </sup>
-                </p>
-              </div>
-              <div className="Col">
-                <p className="PrecipitationToday">
-                  Precipitation: {weatherForecast.precipitation}%
-                </p>
-                <hr />
-                <p className="WindToday">Wind: {weatherForecast.wind} km/h</p>
-                <hr />
-                <p className="PressureToday">
-                  Pressure: {weatherForecast.pressure} mb
-                </p>
-                <hr />
+          <div className="Search">
+            <form>
+              <input
+                type="text"
+                placeholder="Search a city"
+                className="SearchACity"
+              />
+              <input type="submit" value="Let's go!" className="Go" id="go" />
+              <input
+                type="button"
+                value="Current"
+                className="Current"
+                id="current"
+              />
+            </form>
+          </div>
+          <div className="MainCity">
+            <h1 id="search-city">{weatherForecast.city}</h1>
+            <div className="Today">
+              <p className="DateToday">{weatherForecast.date}</p>
+              <p className="Time">{weatherForecast.time}</p>
+            </div>
+            <div className="Container">
+              <div className="Row">
+                <div className="Col">
+                  <img
+                    src={`http://openweathermap.org/img/wn/${weatherForecast.icon}@2x.png`}
+                    alt={weatherForecast.description}
+                    className="ImageWeatherToday"
+                  />
+                </div>
+                <div className="Col">
+                  <p className="TemperatureToday">
+                    {weatherForecast.temperature}
+                    <sup className="CelsiusFahrenheit">°С </sup>
+                  </p>
+                </div>
+                <div className="Col">
+                  <p className="PrecipitationToday">
+                    Humidity: {weatherForecast.humidity}%
+                  </p>
+                  <hr />
+                  <p className="WindToday">Wind: {weatherForecast.wind} km/h</p>
+                  <hr />
+                  <p className="PressureToday">
+                    Pressure: {weatherForecast.pressure} mb
+                  </p>
+                  <hr />
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="Author">
+          <p>
+            This project was created by{" "}
+            <a
+              href="https://eclectic-kringle-cd3e49.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              Olena Gorlenko
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/Gorlenochka?tab=repositories"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              open-sourced on GitHub
+            </a>
+          </p>
+        </div>
       </div>
-      <div className="Author">
-        <p>
-          This project was created by{" "}
-          <a
-            href="https://eclectic-kringle-cd3e49.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            Olena Gorlenko
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/Gorlenochka?tab=repositories"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            open-sourced on GitHub
-          </a>
-        </p>
-      </div>
-    </div>
-  );
+    );
+  }
+  
+  else {
+    const apiKey = "4988ef9330d1f4546a10355cfd9c0b6f";
+    let city = "Paris"
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(url).then(forecastResponse);
+
+    return (<p>Loading...</p>)
+  }
 }
-
-
